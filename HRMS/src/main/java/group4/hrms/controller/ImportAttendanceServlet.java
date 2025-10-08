2432be8d7905b5243e2e8d233ef76e3bc47a8b80
HAB2005 <hoanganhkhuat4869@gmail.com>
2025-10-09 03:07:10 +0700

    AnhKH-modify and add screen  (#8)
    * FE for viewing attendance record for employee and HR
    
    * Modify attendance record for employee screen, import attendance screen
    Add appeal form screen
    
    ---------
    
    Co-authored-by: duongvn9 <156636749+duongvn9@users.noreply.github.com>

--- HRMS/src/main/java/group4/hrms/controller/ImportAttendanceServlet.java
+++ HRMS/src/main/java/group4/hrms/controller/ImportAttendanceServlet.java
@@ -1,26 +1,21 @@
 package group4.hrms.controller;
 
-import java.io.IOException;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+import java.io.IOException;
 
-@WebServlet("/attendance/record/emp")
-public class AttendanceRecordEmpServlet extends HttpServlet{
+@WebServlet("/attendance/import")
+public class ImportAttendanceServlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ArrayList<String> attendanceList = new ArrayList<>();
-        req.setAttribute("attendanceList", attendanceList);
-        req.getRequestDispatcher("/WEB-INF/views/attendance/attendance-record-emp.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/attendance/import-attendance.jsp").forward(req, resp);
     }
 }
